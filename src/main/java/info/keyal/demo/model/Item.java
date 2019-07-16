0a863e84cd5f462104561e236303cc77c0aa1276
Sunil Keyal <sunilkeyal@hotmail.com>
2019-07-16 12:26:19 -0400

    JIRA-101 Added Sales Controller

---
+++ src/main/java/info/keyal/demo/model/Item.java
@@ -0,0 +1,25 @@
+package info.keyal.demo.model;
+
+/**
+ * Item object
+ */
+public class Item {
+    private String name;
+    private double priceInPence;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPriceInPence() {
+        return priceInPence;
+    }
+
+    public void setPriceInPence(double priceInPence) {
+        this.priceInPence = priceInPence;
+    }
+}
--- src/test/java/info/keyal/demo/DemoApplicationTests.java
+++ src/test/java/info/keyal/demo/DemoApplicationTests.java
@@ -5,10 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Application test class
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+    // TODO implement more tests
     @Test
     public void contextLoads() {
     }
