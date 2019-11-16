a0f952c4f770d1512b7e49e38d809c2fe76097cd
Sunil Keyal <sunilkeyal@ziftsolutions.com>
2019-11-15 21:46:19 -0500

    JIRA-120 Method level security, preauthorize only ADMIN can delete customer

--- src/main/java/info/keyal/demo/controller/CustomerController.java
+++ src/main/java/info/keyal/demo/controller/CustomerController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -88,6 +89,7 @@ public class CustomerController {
      * @return void
      */
     @DeleteMapping(value = "/customers/{customerId}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public @ResponseBody
     ResponseEntity<Void> deleteCustomer(@PathVariable Integer customerId) {
         customerService.deleteCustomer(customerId);
