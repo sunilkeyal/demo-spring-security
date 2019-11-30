a5f597dde8e222057bf59c1e60db51f28cc0d114
Sunil Keyal <sunilkeyal@hotmail.com>
2019-11-29 20:26:27 -0500

    JIRA-122 Added login to client UI

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CustomerListComponent } from "./customer-list/customer-list.component";
-import { CustomerFormComponent } from "./customer-form/customer-form.component";
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CustomerListComponent} from "./customer-list/customer-list.component";
+import {CustomerFormComponent} from "./customer-form/customer-form.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
-  { path: 'customers', component: CustomerListComponent},
-  { path: 'addCustomer', component: CustomerFormComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'customers', component: CustomerListComponent},
+  {path: 'addCustomer', component: CustomerFormComponent},
+  {path: '', component: LoginComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,46 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { CustomerFormComponent } from './customer-form/customer-form.component';
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { CustomerService } from "./service/customer.service";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CustomerListComponent} from './customer-list/customer-list.component';
+import {CustomerFormComponent} from './customer-form/customer-form.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CustomerService} from "./service/customer.service";
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerListComponent,
-    CustomerFormComponent
+    CustomerFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [CustomerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     UserListComponent,
-//     UserFormComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     FormsModule
-//   ],
-//   providers: [UserService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
