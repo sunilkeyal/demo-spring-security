674b933025d136bf61c284ace5f396294913a672
Sunil Keyal <sunilkeyal@ziftsolutions.com>
2020-01-03 15:58:32 -0500

    JIRA-125 Fixed test

--- src/main/resources/db/migration/V1_4__Create_demo_tables.sql
+++ src/main/resources/db/migration/V1_4__Create_demo_tables.sql
@@ -7,22 +7,22 @@ create table demo_customer (
     status     varchar(20)  null
 );
 
-create table demo.demo_role (
+create table demo_role (
     id   bigint       not null primary key,
     name varchar(255) null
 );
 
-create table demo.demo_user (
+create table demo_user (
     id       int          not null primary key,
     password varchar(255) null,
     username varchar(255) null
 );
 
-create table demo.demo_user_roles (
+create table demo_user_roles (
     user_id  int    not null,
     roles_id bigint not null,
     constraint UK_172eobe6juyh2bwe8u8n7t2qt unique (roles_id),
-    constraint FKitt3obmd8608gu1kwoo9qd5gs foreign key (roles_id) references demo.demo_role (id),
-    constraint FKkrvnd9yrt3jcp1qcavk9imqo5 foreign key (user_id) references demo.demo_user (id)
+    constraint FKitt3obmd8608gu1kwoo9qd5gs foreign key (roles_id) references demo_role (id),
+    constraint FKkrvnd9yrt3jcp1qcavk9imqo5 foreign key (user_id) references demo_user (id)
 );
 
